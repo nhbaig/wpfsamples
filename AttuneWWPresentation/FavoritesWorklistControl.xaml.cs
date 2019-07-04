83e038c7880dd3e7766573f7f8d8db28e27989ca
pragna majji <pragnajobseeker@gmail.com>
2019-07-04 12:41:56 +0530

    Initial Checkin

---
+++ AttuneWWPresentation/FavoritesWorklistControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AttuneWWPresentation
+{
+    /// <summary>
+    /// Interaction logic for FavoritesWorklistControl.xaml
+    /// </summary>
+    public partial class FavoritesWorklistControl : UserControl
+    {
+        public FavoritesWorklistControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
